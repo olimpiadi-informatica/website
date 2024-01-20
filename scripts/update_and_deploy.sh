a7eef373c720da9dd72fd75651bbd0b46cf77a64
Luca Versari <veluca93@gmail.com>
2024-01-20 23:37:04 +0100

    Ensure multiple builds don't cause a race condition.

--- scripts/update_and_deploy.sh
+++ scripts/update_and_deploy.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 set -xe
-cd "$(dirname "$0")/.."
+
+cd "$(realpath "$(dirname "$0")/..")"
+
+exec 100>/tmp/website_deploy.lock
+flock 100
+
+git checkout .
+git pull
 
 ./scripts/updated_from_git.py
 ./scripts/download_gallery_images.py
 
 # There is no Zola packages for Ubuntu, build using the docker image
-docker run -u "$(id -u):$(id -g)" -v $PWD:/app --workdir /app ghcr.io/getzola/zola:v0.18.0 build
+docker run -u "$(id -u):$(id -g)" -v "$PWD:/app" --workdir /app ghcr.io/getzola/zola:v0.18.0 build
 
 # Copy the output to public-prod. If we're here the build run successfully,
 # so failed builds won't mess up the website
