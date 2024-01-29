5cf015ec647bd77c09e424813e80b5ddfe1cb6d6
Luca Versari <veluca93@gmail.com>
2024-01-29 02:32:32 +0100

    Ensure CD cannot access the network in arbitrary ways.
    Also fix running Python scripts in CD.

--- scripts/download_gallery_images.py
+++ scripts/download_gallery_images.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
 import lib
-import subprocess
-
+import urllib3
 import os
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+NUM_THREADS = 32
+
+http = urllib3.PoolManager(num_pools=NUM_THREADS)
 
 
-os.chdir(os.path.join(os.path.dirname(__file__), ".."))
+def fetch_and_save(url_and_dest) -> None:
+    url, dest = url_and_dest
+    print(f"Downloading {url} to {dest}", file=sys.stderr)
+    response = http.request("GET", url)
+    with open(dest, 'wb') as f:
+        f.write(response.data)
 
-children = []
+
+urls_and_dests = []
 
 for post in lib.all_posts():
     if 'extra' in post.header and 'gallery' in post.header['extra']:
-        d = os.path.join("static", "images", "gallery", post.name())
+        d: str = os.path.join("static", "images", "gallery", post.name())
         os.makedirs(d, exist_ok=True)
         for info in post.header['extra']['gallery']:
-            fname = info['filename']
-            dest = os.path.join(d, fname)
+            url = urllib3.util.parse_url(info['url'])
+            assert url.netloc == "media.olimpiadi-informatica.it"
+            assert url.scheme == "https"
+            fname = info['filename'].replace("/", "")
+            dest: str = os.path.join(d, fname)
             if not os.path.exists(dest):
-                children.append(subprocess.Popen(
-                    ["wget", "-4", "-O", dest, "-o", "/dev/null", info['url']]
-                ))
+                urls_and_dests.append((info['url'], dest))
 
-for child in children:
-    child.wait()
+ThreadPoolExecutor(max_workers=32).map(fetch_and_save, urls_and_dests)
